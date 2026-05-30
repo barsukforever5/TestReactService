4f2300a5d50c59449a7116cc1c5c3004535fa804
Alexander Makanin <makanin@netcracker.com>
2026-05-30 17:39:19 +0300

    minus CORS

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -10,10 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = {
-        "http://localhost:3000",
-        "http://localhost:5173"
-})
+@CrossOrigin(origins = {"*"})
 
 @RestController
 @RequestMapping("/api")
